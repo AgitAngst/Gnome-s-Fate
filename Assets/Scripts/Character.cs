2a27b734264febd7be1c87a55f15849f5ff40c4f
Nikolay Vasyukov <agitangst@gmail.com>
2021-12-29 13:30:48 +0300

    Strafes are working correctly now

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG;
 using DG.Tweening;
+using DG.Tweening.Core;
+using DG.Tweening.Plugins.Options;
 using TMPro;
 using UnityEngine.Serialization;
 
@@ -15,12 +17,12 @@ public class Character : MonoBehaviour
     private bool keyPressed = false;
     private Animator animator;
     private States state;
-    public States.CharacterState _states;
-    private Ground _ground;
+    [FormerlySerializedAs("_states")] public States.CharacterState states;
+    private Ground ground;
     public GameObject groundObject;
     private float tmpSpeed;
     public float keyPressTimer = 0.5f;
-    private ObjectsSpawner _objectsSpawner;
+    private ObjectsSpawner objectsSpawner;
     private float positionSum;
     private float animatorBlendValue;
     private bool isDead = false;
@@ -28,6 +30,12 @@ public class Character : MonoBehaviour
     private float test1;
     private float lerpTimeElapsed;
     public float lerpDuration = 0.5f;
+    private static readonly int RunDirection = Animator.StringToHash("RunDirection");
+    private static readonly int Die = Animator.StringToHash("Die");
+    private static readonly int Attack1 = Animator.StringToHash("Attack");
+    private static readonly int IsRunning = Animator.StringToHash("isRunning");
+    private TweenerCore<float, float, FloatOptions> tweener;
+
 
     public CharacterEvents characterEvents;
 
@@ -36,29 +44,31 @@ public class Character : MonoBehaviour
     public LayerMask layerMask;
 
     public int attackDamage;
-    public int  maxHP;
-    public TextMeshProUGUI playerHP;
-    private int currentHP;
+    [FormerlySerializedAs("maxHP")] public int maxHp;
+    [FormerlySerializedAs("playerHP")] public TextMeshProUGUI playerHp;
+    private int currentHp;
+    private float blendDirectionValue;
+    private static readonly int Hurt = Animator.StringToHash("Hurt");
+
     void Start()
     {
-        _states = States.CharacterState.Idle;
+        states = States.CharacterState.Idle;
         animator = GetComponentInChildren<Animator>();
         tmpSpeed = characterSpeed;
-        _ground = groundObject.GetComponent<Ground>();
+        ground = groundObject.GetComponent<Ground>();
         positionSum = 0f;
-        currentHP = maxHP;
-        playerHP.text = currentHP.ToString();
-
+        currentHp = maxHp;
+        playerHp.text = currentHp.ToString();
     }
 
     void Update()
     {
         CharacterControl();
-        switch (_states)
+        switch (states)
         {
             case States.CharacterState.Idle:
                 Idle();
-                _ground.degreesPerSecond = 0f;
+                ground.degreesPerSecond = 0f;
                 break;
             case States.CharacterState.Running:
                 Run();
@@ -84,16 +94,7 @@ public class Character : MonoBehaviour
             }
 
             transform.DOMove(new Vector3(positionSum, 0, 0), 1f);
-
-            // if (lerpTimeElapsed < lerpDuration)
-            // {
-            //     animator.SetFloat("RunDirection", Mathf.Lerp(0f, -1f, lerpTimeElapsed / lerpDuration));
-            //     lerpTimeElapsed += Time.deltaTime;
-            // }
-
-            StartCoroutine("AnimatorLerp");
-
-            //animator.SetFloat("RunDirection", -1f);
+            AnimatorLerp(-1);
         }
 
         if (Input.GetKeyDown(KeyCode.D))
@@ -103,10 +104,11 @@ public class Character : MonoBehaviour
             {
                 positionSum = 3f;
             }
-            transform.DOMove(new Vector3(positionSum ,0 , 0) , 1f);
-            animator.SetFloat("RunDirection", 1f);
 
+            transform.DOMove(new Vector3(positionSum, 0, 0), 1f);
+            AnimatorLerp(1);
         }
+
         // if (Input.GetKey(KeyCode.D) && transform.position.x < 3)
         // {
         //     xPosition = transform.position.x;
@@ -115,24 +117,27 @@ public class Character : MonoBehaviour
         // }
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            animator.SetTrigger("Attack");
+            animator.SetTrigger(Attack1);
             //attack is an event in attack animation
         }
+
         if (Input.GetKey(KeyCode.Space))
         {
-            _states = States.CharacterState.Running;
+            states = States.CharacterState.Running;
         }
     }
 
-   private IEnumerator AnimatorLerp()
+    private void AnimatorLerp(float direction, float duration = 0.5f)
     {
-        test1 = animator.GetFloat("RunDirection");
-      //  DOTween.To(() => test1, x => test1 = x, -1f, .5f);
-      DOTween.To(() => test1, x => test1 = x, -1f, .5f);
-  
-      animator.SetFloat("RunDirection",test1);
-        Debug.Log("spam");
-        yield return null;
+        tweener?.Kill();
+        blendDirectionValue = animator.GetFloat(RunDirection);
+        tweener = DOTween.To(() => blendDirectionValue, x => blendDirectionValue = x, direction, duration)
+            .SetEase(Ease.InOutBounce)
+            .OnUpdate(() => { animator.SetFloat(RunDirection, blendDirectionValue); })
+            .OnComplete(() =>
+            {
+                if (direction != 0) AnimatorLerp(0);
+            });
     }
 
     public void Attack()
@@ -148,41 +153,41 @@ public class Character : MonoBehaviour
     public void Idle()
     {
         characterSpeed = 0f;
-        _ground.degreesPerSecond = characterSpeed;
-
+        ground.degreesPerSecond = characterSpeed;
     }
+
     public void TakeDamage(int damage)
     {
-        currentHP -= damage;
-        playerHP.text = currentHP.ToString();
-        if (currentHP >= 1)
+        currentHp -= damage;
+        playerHp.text = currentHp.ToString();
+        if (currentHp >= 1)
         {
-            animator.SetTrigger("Hurt");
-
+            animator.SetTrigger(Hurt);
         }
         else
         {
-            _states = States.CharacterState.Dead;
+            states = States.CharacterState.Dead;
             Death();
         }
     }
+
     public void Death()
     {
         characterSpeed = 0f;
-        _ground.degreesPerSecond = characterSpeed;
+        ground.degreesPerSecond = characterSpeed;
         isDead = true;
-        animator.SetTrigger("Die");
+        animator.SetTrigger(Die);
     }
 
     public void Run()
     {
-        animator.SetBool("isRunning",true);
+        animator.SetBool(IsRunning, true);
         characterSpeed = tmpSpeed;
 
-        _ground.degreesPerSecond = characterSpeed;
+        ground.degreesPerSecond = characterSpeed;
     }
 
-    
+
     void KeyPressTimer()
     {
         if (keyPressed)
@@ -190,12 +195,9 @@ public class Character : MonoBehaviour
             keyPressTimer -= Time.deltaTime;
             if (keyPressTimer <= 0f)
             {
-                
                 keyPressed = false;
-            } 
+            }
         }
-       
-        
     }
 
     void ResetBlend()
@@ -209,6 +211,7 @@ public class Character : MonoBehaviour
         {
             return;
         }
-        Gizmos.DrawWireSphere(attackPoint.transform.position,attackRange);
+
+        Gizmos.DrawWireSphere(attackPoint.transform.position, attackRange);
     }
 }
