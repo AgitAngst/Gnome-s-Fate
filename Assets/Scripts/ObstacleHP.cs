1c9e9cb0807580eb254dbce065b5ad29d32eb008
Nikolay Vasyukov <agitangst@gmail.com>
2022-01-01 15:28:48 +0300

    Added first version of enemy movement between lines

--- Assets/Scripts/ObstacleHP.cs
+++ Assets/Scripts/ObstacleHP.cs
@@ -1,23 +1,31 @@
 using UnityEngine;
 using UnityEngine.Serialization;
 using Random = UnityEngine.Random;
+using DG.Tweening;
 
 public class ObstacleHp : MonoBehaviour
 {
     [FormerlySerializedAs("maxHP")] public int maxHp = 1;
     public bool getDistance = false;
+    private bool canJump = true;
+    private bool isStayingInPlace = false;
     private int currentHp;
     public float scorePerObject;
     private Character character;
     private GameManager gameManager;
     private Animator animator;
     public float distanceToReact = 3f;
+    public float distanceToMove = 10f;
     private float distance;
     private AudioManager audioManager;
     private static readonly int Attack = Animator.StringToHash("Attack");
     public States.ObstacleType obstacleType;
-    public SpawnedLineEnum spawnedLine;
-    public enum SpawnedLineEnum
+    [FormerlySerializedAs("spawnedLine")] public EnemyLineEnum enemyLine;
+    private int playerLine;
+    private int nextLineToJump;
+    private static readonly int IsJumping = Animator.StringToHash("isJumping");
+
+    public enum EnemyLineEnum
     {
         Left,
         Center,
@@ -30,6 +38,7 @@ public class ObstacleHp : MonoBehaviour
         {
             case States.ObstacleType.Enemy:
                 EnemyAttack();
+                JumpOverTheLine();
                 break;
             case States.ObstacleType.Obstacle:
                 break;
@@ -46,7 +55,8 @@ public class ObstacleHp : MonoBehaviour
         {
             animator = gameObject.GetComponent<Animator>();
         }
-        CheckSpawnedLine();
+
+        CheckEnemyLine();
     }
 
     public void Damage(int damage)
@@ -85,23 +95,111 @@ public class ObstacleHp : MonoBehaviour
         }
     }
 
-    void CheckSpawnedLine()
+    void CheckEnemyLine()
     {
         var lines = transform.position.x;
 
         if (lines <= -3f)
         {
-            spawnedLine  = SpawnedLineEnum.Left;
+            enemyLine = EnemyLineEnum.Left;
         }
         else if (lines >= 3f)
 
         {
-            spawnedLine = SpawnedLineEnum.Right;
+            enemyLine = EnemyLineEnum.Right;
         }
         else
         {
-            spawnedLine = SpawnedLineEnum.Center;
-
+            enemyLine = EnemyLineEnum.Center;
         }
+    }
+
+    void JumpOverTheLine()
+    {
+        switch (enemyLine)
+        {
+            case EnemyLineEnum.Center:
+                if (GetDistanceToPlayer() <= distanceToMove && canJump)
+                {
+                    JumpToAnotherLine();
+                }
+
+                break;
+            case EnemyLineEnum.Left:
+                break;
+            case EnemyLineEnum.Right:
+                break;
+        }
+    }
+
+    private void JumpToAnotherLine()
+    {
+        CheckEnemyLineToJump();
+        animator.SetBool(IsJumping, true);
+        var position = transform.position;
+        transform.DOMove(new Vector3(nextLineToJump, position.y, position.z), .5f).SetEase(Ease.InOutExpo).OnComplete(
+            () =>
+            {
+                animator.SetBool(IsJumping, false);
+                canJump = false;
+                CheckEnemyLine();
+            });
+    }
+
+    private int CheckEnemyLineToJump()
+    {
+        isStayingInPlace = Random.value <= .5f;
+        switch (enemyLine)
+        {
+            case EnemyLineEnum.Center:
+                if (!isStayingInPlace)
+                {
+                    if (Random.value <= 0.5f)
+                    {
+                        nextLineToJump = -3;
+                    }
+                    else nextLineToJump = 3;
+                }
+                break;
+            case EnemyLineEnum.Right:
+                if (!isStayingInPlace)
+                {
+                    if (Random.value <= 0.5f)
+                    {
+                        nextLineToJump = -3;
+                    }
+                    else nextLineToJump = 0;
+                }
+                break;
+            case EnemyLineEnum.Left:
+            {
+                if (Random.value <= 0.5f)
+                {
+                    nextLineToJump = 0;
+                }
+                else nextLineToJump = 3;
+            }
+                break;
+        }
+
+        return nextLineToJump;
+    }
+
+    int CheckPlayerLine()
+    {
+        switch (character.characterLine)
+        {
+            case States.CharacterLine.Left:
+                playerLine = -1;
+                break;
+            case States.CharacterLine.Right:
+                playerLine = 1;
+                break;
+            case States.CharacterLine.Center:
+                playerLine = 0;
+                break;
+        }
+
+        return playerLine;
     }
 }
